60bfc5ff08a3eca4a8413c73ec40216c1f8dd860
Wesley Bonneville <westazbonneville@gmail.com>
2019-11-05 14:41:25 -0500

    add services, title

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,23 @@ import React from 'react';
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
+import Services from '../components/Services';
 
 export default function Home() {
   return (
-    <Hero>
-      {/* add props */}
-      <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
-        <Link to="/rooms" className="btn-primary" />
-      </Banner>
-    </Hero>
+    <React.Fragment>
+      <Hero>
+        {/* add props */}
+        <Banner
+          title="luxurious rooms"
+          subtitle="deluxe rooms starting at $299"
+        >
+          <Link to="/rooms" className="btn-primary">
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+    </React.Fragment>
   );
 }
