28732943de71f05d4dd978071a1b809d5a142e9b
Wesley Bonneville <westazbonneville@gmail.com>
2019-11-06 10:57:12 -0500

    Add section for featured rooms

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import { RoomContext } from '../context';
 import Loading from './Loading';
-
+import Room from '../components/Room';
+import Title from '../components/Title';
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
-    const { featuredRooms: rooms } = this.context;
-    console.log(rooms);
+    let { loading, featuredRooms: rooms } = this.context;
+    rooms = rooms.map(room => {
+      return <Room key={room.id} room={room} />;
+    });
     return (
-      <div>
-        hello from featured rooms
-        <Loading />
-      </div>
+      <section className="featured-rooms">
+        <Title title="featured room" />
+        <div className="featured-rooms-center">
+          {loading ? <Loading /> : rooms}
+        </div>
+      </section>
     );
   }
 }
