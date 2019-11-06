161b0dceef505b1cd685311166c71b94d39213d9
Wesley Bonneville <westazbonneville@gmail.com>
2019-11-06 10:20:47 -0500

    add featuredRooms

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -4,6 +4,8 @@ import { RoomContext } from '../context';
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
+    const { featuredRooms: rooms } = this.context;
+    console.log(rooms);
     return <div>hello from featured rooms</div>;
   }
 }
