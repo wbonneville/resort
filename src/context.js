540720fd2fe5a423f2d465ca31b02e29be522c01
Wesley Bonneville <westazbonneville@gmail.com>
2019-11-06 10:12:01 -0500

    Add lifecycle method, formatData

--- src/context.js
+++ src/context.js
@@ -9,6 +9,23 @@ class RoomProvider extends Component {
     featuredRooms: [],
     loading: true,
   };
+  // getData
+
+  componentDidMount() {
+    let rooms = this.formatData(items);
+    console.log(rooms);
+  }
+
+  formatData(items) {
+    let tempItems = items.map(item => {
+      let id = item.sys.id;
+      let images = item.fields.images.map(image => image.fields.file.url);
+      let room = { ...item.fields, images: images, id };
+      return room;
+    });
+    return tempItems;
+  }
+
   render() {
     return (
       <RoomContext.Provider value={{ ...this.state }}>
