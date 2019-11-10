1710849bf376a8566513e0627246f808a5cd177e
Wesley Bonneville <westazbonneville@gmail.com>
2019-11-10 18:44:01 -0500

    Update SingleRoom.js

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -68,7 +68,12 @@ export default class SingleRoom extends Component {
             </article>
             <article className="info">
               <h3>info</h3>
-              <h6>price : $ {price}</h6>
+              <h6>price : ${price}</h6>
+              <h6>size: {size}</h6>
+              <h6>
+                max capacity :{' '}
+                {capacity > 1 ? `${capacity} people` : `${capacity} person `}
+              </h6>
             </article>
           </div>
         </section>
