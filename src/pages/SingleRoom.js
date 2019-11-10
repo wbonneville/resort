1127a065dc09222b08f92cd75b818f67c0fa3ee3
Wesley Bonneville <westazbonneville@gmail.com>
2019-11-10 18:49:19 -0500

    Add extras

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -74,8 +74,18 @@ export default class SingleRoom extends Component {
                 max capacity :{' '}
                 {capacity > 1 ? `${capacity} people` : `${capacity} person `}
               </h6>
+              <h6>{pets ? 'pets allowed' : 'no pets allowed'}</h6>
+              <h6>{breakfast && 'free breakfast included'}</h6>
             </article>
           </div>
+        </section>
+        <section className="room-extras">
+          <h6>extras</h6>
+          <ul className="extras">
+            {extras.map((item, index) => {
+              return <li key={index}>-{item}</li>;
+            })}
+          </ul>
         </section>
       </>
     );
