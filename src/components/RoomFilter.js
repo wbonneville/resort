5fc2cfedc8f514f5ec95e1b65ac1e9e49a9bf194
Wesley Bonneville <westazbonneville@gmail.com>
2019-11-11 12:35:13 -0500

    Add room filter

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,5 +1,46 @@
 import React from 'react';
+import { useContext } from 'react';
+import { RoomContext } from '../context';
+import Title from '../components/Title';
 
-export default function RoomFilter() {
-  return <div>hi from room filter</div>;
+// get all unique values
+const getUnique = (items, value) => {
+  return [...new Set(item.map(item => item[value]))];
+};
+
+export default function RoomFilter({ rooms }) {
+  const context = useContext(RoomContext);
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+  } = context;
+
+  let types = getUnique(rooms, 'type');
+
+  return (
+    <section className="filter-container">
+      <Title title="search rooms" />
+      <form action="" className="filter-form">
+        {/* select type */}
+        <div className="form-group">
+          <label htmlFor="type">room type</label>
+          <select
+            name="type"
+            id=""
+            value={type}
+            className="form-control"
+            onChange={handleChange}
+          ></select>
+        </div>
+        {/* end select type */}
+      </form>
+    </section>
+  );
 }
